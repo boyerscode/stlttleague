8d351cfb66a10602bc2dca358326b04b1452731b
boyerscode <30.jboyer@gmail.com>
2023-06-16 10:39:38 -0500

    Updated layout

---
+++ src/lib/components/Navbar.svelte
@@ -0,0 +1,32 @@
+<header class="col-span-3 bg-daisyBush text-center">
+  <div>
+    <a href="/invoices">
+      <img src="/images/logo.jpeg" alt="Super League" class="mx-auto" />
+    </a>
+  </div>
+  <nav>
+    <ul class="list-none text-2xl font-bold">
+      <li>
+        <a href="/invoices">Invoices</a>
+      </li>
+      <li>
+        <a href="/clients">Clients</a>
+      </li>
+      <li>
+        <a href="#">Settings</a>
+      </li>
+      <li>
+        <a href="#">Logout</a>
+      </li>
+    </ul>
+  </nav>
+</header>
+
+<style lang="postcss">
+  nav ul li {
+    @apply mb-6;
+  }
+  nav ul li a {
+    @apply font-bold text-white hover:text-goldenFizz;
+  }
+</style>
